f483b7b9fa87cebfa407c2197b74800b983aae37
TankorSmash <tankorsmash@gmail.com>
2013-06-30 13:56:26 -0400

    Moved more stuff out of main into utils

--- FirstCPPApplication/FirstCPPApplication.cpp
+++ FirstCPPApplication/FirstCPPApplication.cpp
@@ -5,6 +5,7 @@
 #include <cstdlib>
 #include <algorithm>
 
+#include "utils.h"
 #include "Person.h"
 #include "Pet.h"
 #include "FirstCPPApplication.h"
@@ -18,54 +19,6 @@ void WelcomeMessage(){
     // printf("\n\n");
     printf("\n\t\t   Prepare to die!\n\n");
 
-};
-
-std::string ToLower(std::string data){
-	std::transform(data.begin(), data.end(), data.begin(), ::tolower);
-    return data;
-};
-
-std::string ToUpper(std::string data){
-	std::transform(data.begin(), data.end(), data.begin(), ::toupper);
-    return data;
-};
-
-
-void print_str_array(std::string msg_list[], int len){
-
-    std::cout << len << std::endl;
-    for (int i = 0; i < len; i++){
-    std::cout << msg_list[i] << std::endl;
-    };
-};
-
-int ask_for_int(std::string msg, int default_int){
-
-    // std::cout << msg << std::endl;
-    std::cout << msg << std::endl << ">>> ";
-    std::string str_answer;
-    getline(std::cin, str_answer);
-    if (str_answer == ""){
-    return default_int;
-    }
-    else {
-    int int_answer = atoi(str_answer.c_str());
-    return int_answer;
-    }
-};
-
-std::string ask_for_str(std::string msg, std::string default_str){
-
-    std::cout << msg << std::endl << ">>> ";
-    std::string str_answer;
-    getline(std::cin, str_answer);
-
-    if (str_answer == ""){
-    return default_str;
-    }
-    else {
-    return str_answer;
-    }
 };
 
 int main ()
--- FirstCPPApplication/FirstCPPApplication.h
+++ FirstCPPApplication/FirstCPPApplication.h
@@ -1,12 +1,5 @@
 
 #pragma once
-
 #include <sstream>
 
 void WelcomeMessage();
-std::string ToLower(std::string data);
-std::string ToUpper(std::string data);
-void print_str_array(std::string msg_list[], int len);
-int ask_for_int(std::string msg, int default_int=0);
-std::string ask_for_str(std::string msg, std::string default_str="STRING");
-
