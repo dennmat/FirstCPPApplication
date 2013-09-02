6acece6e5044bc15790a154c133f1036456aa754
TankorSmash <tankorsmash@gmail.com>
2013-09-02 19:12:17 -0400

    Added Room class base

---
+++ FirstCPPApplication/Room.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+
+#include "Room.h"
+
+Room::Room()
+{
+    this->width = -1;
+    this->height = -1;
+
+    this->x = -1;
+    this->y = -1;
+};
---
+++ FirstCPPApplication/Room.h
@@ -0,0 +1,19 @@
+#ifndef ROOM_H
+#define ROOM_H
+
+class Room
+{
+    public:
+        Room();
+        ~Room();
+
+        //w and h of the room
+        int width, height;
+
+        //its location on the map
+        int x, y;
+
+        
+};
+
+#endif
