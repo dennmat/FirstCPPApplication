f6e9cfe49d6edc52216b0bbf6f50ce1eedb46c62
TankorSmash <tankorsmash@gmail.com>
2013-10-05 01:02:57 -0400

    more changes

--- FirstCPPApplication/thinker.cpp
+++ FirstCPPApplication/thinker.cpp
@@ -31,38 +31,30 @@ void Thinker::update(Game* game)
             cout << "Building Path" << endl;
             master->l_path = new TCODPath(map->l_map);
 
-            TCODRandom * rnd = TCODRandom::getInstance();
+            // TCODRandom * rnd = TCODRandom::getInstance();
             // master->dest_x = rnd->getInt(1, map->width-1);
             // master->dest_y = rnd->getInt(1, map->height-1);
-            // master->dest_x = rnd->getInt(1, 3);
-            // master->dest_y = rnd->getInt(1, 3);
             master->dest_x = game->player->x;
-            master->dest_y = game->player->y;
-            // master->dest_x = 6;
-            // master->dest_y = 3;
-            cout << master->x << ", " <<  master->y << ", " <<  master->dest_x << ", " <<  master->dest_y << ", " << endl;
+            master->dest_y = game->player->y-1;
             int x, y, dest_x, dest_y;
-            x = master->x;
-            y = master->y;
-            dest_x = master->dest_x;
-            dest_y = master->dest_y;
-            cout << x << ", " <<  y << ", " <<  dest_x << ", " <<  dest_y << ", " << endl;
-            master->l_path->compute(x, y, dest_x, dest_y);
-             // master->l_path->compute(4,9,1,3);
+            x = master->x; y = master->y;
+            dest_x = master->dest_x; dest_y = master->dest_y;
+            bool computed = master->l_path->compute(x, y, dest_x, dest_y);
+            // master->l_path->compute(4,9,1,3);
             int path_size = master->l_path->size();
             cout << "Path size AFTER BUILDING IT: " << path_size << endl << "I'mna walk it" << endl;
         };
-            int path_size = master->l_path->size();
-            cout << "Path size: " << path_size << endl << "I'mna walk it" << endl;
-            master->l_path->walk(&master->x, &master->y, true);
-            Tile * next_tile = map->getTileAt(master->x,master->y);
-            master->putPerson(next_tile, master->x, master->y); 
+        int path_size = master->l_path->size();
+        cout << "Path size: " << path_size << endl << "I'mna walk it" << endl;
+        master->l_path->walk(&master->x, &master->y, true);
+        Tile * next_tile = map->getTileAt(master->x,master->y);
+        master->putPerson(next_tile, master->x, master->y); 
 
-            if (path_size == 0)
-            {
-                delete master->l_path;
-                master->l_path = NULL;
-            }
+        //if (path_size == 0)
+        //{
+        delete master->l_path;
+        master->l_path = NULL;
+        //}
     }
 
 };
