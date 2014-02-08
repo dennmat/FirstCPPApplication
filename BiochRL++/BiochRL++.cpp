3f519a32ddb6e7ed96c0a579d2a614ba6d4806e5
TankorSmash <tankorsmash@gmail.com>
2014-02-08 16:59:44 -0500

    indentation

--- BiochRL++/BiochRL++.cpp
+++ BiochRL++/BiochRL++.cpp
@@ -6,17 +6,15 @@
 #include "map.h"
 
 
-	//Game the_game;
 int _tmain(int argc, _TCHAR* argv[])
 {
-auto map = Game::buildworld();
-Game::current_map = map;
-//Map* Game::current_map = world; //I'm not so sure about this, but it solved the mem issue
+    auto map = Game::buildworld();
+    Game::current_map = map;
 
-Game::initialize_player(); //created the Person player
-Game::initialize_enemies(); // create the enemies
-Game::initialize_items(); // create the items
-	Game::mainloop();
-	return 0;
+    Game::initialize_player(); //created the Person player
+    Game::initialize_enemies(); // create the enemies
+    Game::initialize_items(); // create the items
+    Game::mainloop();
+    return 0;
 }
 
