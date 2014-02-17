3ed041937c2e6fb69189643a9d224900c8e6ee27
TankorSmash <tankorsmash@gmail.com>
2014-02-17 17:44:01 -0500

    Lowered player hp
    The game is a shit ton more fun, but gets a lot easier. Need to work out
    how to make the game more challenging

--- FirstCPPApplication/game.cpp
+++ FirstCPPApplication/game.cpp
@@ -348,6 +348,8 @@ Person*  Game::initialize_player()
     player = new Person("Josh", 23, 3, 3, '@', "PLAYER ONE");
     // player->representation->fg_color = &(TCODColor)(TCODColor::celadon);
     player->representation->setFGColor(TCODColor::celadon, true, true, true);
+    player->attrs->health->current_val=50;
+    player->attrs->health->max_val=50;
     player->attrs->damage->current_val=10;
     delete player->thinker;
     player->thinker = NULL;
