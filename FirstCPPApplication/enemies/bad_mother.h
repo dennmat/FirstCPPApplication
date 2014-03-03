383f8ed14caf78f5d65c7de44bc297d2cd1e6c87
TankorSmash <tankorsmash@gmail.com>
2014-03-02 19:07:36 -0500

    Added bad_mother boss character

---
+++ FirstCPPApplication/enemies/bad_mother.h
@@ -0,0 +1,15 @@
+#ifndef BAD_MOTHER_H
+#define BAD_MOTHER_H
+
+#include <actors\Person.h>
+
+class BadMother : public Person
+{
+    public:
+        BadMother(std::string name, int age, int x, int y, char repr, std::string combat_name);
+        static const int BadMother::pack_size = 1;
+        void championize();
+
+};
+
+#endif
