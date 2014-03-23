d559772507d1acae3c0d00649adf32a814fab33f
TankorSmash <tankorsmash@gmail.com>
2014-03-23 00:54:07 -0400

    added more files

---
+++ FirstCPPApplication/enums/screens.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef SCREENS_H
+#define SCREENS_H
+
+enum Screens {
+    InventoryScreen,
+    CharacterSheetScreen,
+    MainMenuScreen,
+    HelpScreen,
+    SpellSelectScreen
+};
+#endif
